d5053eac2cba6753255991a1ac99b914a69f2505
Amin <min123kabir@gmail.com>
2025-04-14 03:28:43 +0100

    market cap check

--- bot.ts
+++ bot.ts
@@ -1,5 +1,6 @@
 import { Bot } from "grammy";
 import env from "dotenv";
+import axios from "axios"
 
 env.config();
 
@@ -14,8 +15,37 @@ const bot = new Bot(process.env.TELEGRAM_TOKEN);
 
 // Handle the /start command.
 bot.command("start", (ctx) => ctx.reply("Welcome slime! Up and running."));
+
+bot.on("message", async (ctx) => {
+  const address = ctx.message.text
+  try {
+    const { data } = await axios.get(`https://api.coingecko.com/api/v3/coins/solana/contract/${address}`)
+    const market_cap = data.market_data.market_cap.usd
+    const price = data.market_data.current_price.usd
+    const volume = data.market_data.total_volume.usd
+    const liquidity = data.market_data.liquidity_score
+    const holders = data.market_data.holders
+    const holders_count = data.market_data.holders_count
+    ctx.reply(`Market Cap: ${market_cap}\nPrice: ${price}\nVolume: ${volume}\nLiquidity: ${liquidity}\nHolders: ${holders}\nHolders Count: ${holders_count}`)
+  } catch (error) {
+    ctx.reply("invalid address")
+  }
+});
+
+bot.hears("search", async () => {
+  const response = await axios.get("https://api.coingecko.com/api/v3/coins/solana/contract/6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN", {
+    headers: {
+      "x-cg-api-key": process.env.COIN_API_KEY
+    }
+  })
+  const result = response.data
+  const {market_cap} = result
+  console.log(result)
+
+})
 // Handle other messages.
 bot.on("message", (ctx) => ctx.reply("Got another message!"));
+
 
 // Now that you specified how to handle messages, you can start your bot.
 // This will connect to the Telegram servers and wait for messages.
